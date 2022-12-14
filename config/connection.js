62e0a3bd1f52135f9929e31c3507b5a3c4f0c5fe
chilikilong112 <chiliki.long@gmail.com>
2022-12-14 21:37:43 +1100

    make it better

--- config/connection.js
+++ config/connection.js
@@ -4,8 +4,15 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
+  sequelize = new Sequelize(database, 'postgres', '', {
+    dialect: 'postgres'
+  });
+}
+
+// {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// }
+else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
