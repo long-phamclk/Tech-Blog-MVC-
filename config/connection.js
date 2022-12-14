8991e63fd751f6cf861b1cc05bcc5c39d381a850
chilikilong112 <chiliki.long@gmail.com>
2022-12-14 21:57:18 +1100

    make it better

--- config/connection.js
+++ config/connection.js
@@ -3,28 +3,34 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  // the application is executed on Heroku ... use the postgres database
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    protocol: 'postgres',
-    logging: true //false
-  });
-}
+// if (process.env.JAWSDB_URL)
+// // the application is executed on Heroku ... use the postgres database
 // {
 //   sequelize = new Sequelize(process.env.JAWSDB_URL);
 // }
-else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
+// else {
+//   sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       port: 3306
+//     }
+//   );
+// }
+
+if (process.env.DATABASE_URL) {
+  // the application is executed on Heroku ... use the postgres database
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    logging:  true //false
+  });
+} else {
+  // the application is executed on the local machine
+  sequelize = new Sequelize("postgres:///my_db");
 }
 
 module.exports = sequelize;
